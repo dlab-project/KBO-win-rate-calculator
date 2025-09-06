27fb8532f88a50abcd4a258c8780ca4c7e74ce28
j5ng <alswhddh@naver.com>
2025-09-06 18:54:50 +0900

    팀 통계 CSV 파일에 새로운 열 추가 및 데이터 형식 수정

---
+++ web/database.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://avdxuubamsjuwwojqonb.supabase.co'
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2ZHh1dWJhbXNqdXd3b2pxb25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3MjI1MjcsImV4cCI6MjA3MDI5ODUyN30.KvZZyC44-0_scfXVv55S9DGskCFmBP378Ke_ZwUUi9w"
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
